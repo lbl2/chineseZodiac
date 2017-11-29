88b250a5d23d02805e74dd975fbbf27985f25ce6
lbl2 <32145519+lbl2@users.noreply.github.com>
2017-11-29 16:36:57 -0600

    Add files via upload

---
+++ Animals.java
@@ -0,0 +1,53 @@
+
+public class Animals {
+	
+	public static String setZodiac(int year) {
+		if (year == 1984 || year == 1996) {
+			return "rat";
+		}
+		
+		if (year == 1985 || year == 1997) {
+			return "ox";		
+			}
+		
+		if (year == 1986 || year == 1998) {
+			return "tiger";		
+			}
+		
+		if (year == 1987 || year == 1999) {
+			return "rabbit";		
+			}
+		
+		if (year == 1988 || year == 2000) {
+			return "dragon";		
+			}
+		
+		if (year == 1989 || year == 2001) {
+			return "snake";
+		}
+		
+		if (year == 1990 || year == 2002) {
+			return "horse";
+		}
+		
+		if (year == 1991 || year == 2003) {
+			return "sheep";
+		}
+		
+		if (year == 1992 || year == 2004) {
+			return "monkey";
+		}
+		
+		if (year == 1993 || year == 2005) {
+			return "rooster";
+		}
+		
+		if (year == 1994 || year == 2006) {
+			return "dog";
+		}
+		
+		if (year == 1995 || year == 2007) {
+			return "pig";
+		}
+	}
+}
